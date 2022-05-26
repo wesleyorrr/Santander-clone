e48611188a53464168baca1705dd0e19eef12490
wesle_p1v78x7 <wesleyorrr@gmail.com>
2022-05-26 11:30:39 -0300

    👨‍💻 app em contrução de telas

---
+++ app/src/main/java/br/com/map/santander/ui/MenuActivity.kt
@@ -0,0 +1,12 @@
+package br.com.map.santander.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.map.santander.R
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
